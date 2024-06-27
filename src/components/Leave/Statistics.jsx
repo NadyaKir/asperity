84f561476bcbb6c78e11e7054215b49d86c7227c
NadyaKir <styyypik@gmail.com>
2024-06-27 11:30:12 +0600

    styles: tooltip

--- src/components/Leave/Statistics.jsx
+++ src/components/Leave/Statistics.jsx
@@ -8,7 +8,32 @@ export default function Statistics() {
     <div className="p-[30px]">
       <div className="flex items-center">
         <Heading extra="mr-2">Статистика</Heading>
-        <Tooltip title="1 раб. месяц = 3 дня отпуска" placement="right">
+        <Tooltip
+          title="1 раб. месяц = 3 дня отпуска"
+          placement="right"
+          slotProps={{
+            popper: {
+              modifiers: [
+                {
+                  name: "offset",
+                  options: {
+                    offset: [0, -7],
+                  },
+                },
+              ],
+            },
+          }}
+          PopperProps={{
+            sx: {
+              "& .MuiTooltip-tooltip": {
+                backgroundColor: "#303030",
+                color: "#E1E3E6",
+                padding: "6px 10px",
+                fontWeight: "500",
+              },
+            },
+          }}
+        >
           <span>
             <Info />
           </span>
