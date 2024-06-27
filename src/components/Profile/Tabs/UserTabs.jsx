9abbac3c5dca2d2461bd8dc62192626b17b688e3
NadyaKir <styyypik@gmail.com>
2024-06-27 11:10:35 +0600

    styles: userTabs

--- src/components/Profile/Tabs/UserTabs.jsx
+++ src/components/Profile/Tabs/UserTabs.jsx
@@ -3,14 +3,14 @@ import { tabs } from "../../../../json";
 
 export default function UserTabs({ value, onTabChange }) {
   return (
-    <div className="inline-block mb-5">
+    <div className="inline-block mb-5 font-">
       <Tabs
         value={value}
         onChange={onTabChange}
         aria-label="user tabs"
         sx={{
           "& .MuiTabs-indicator": {
-            backgroundColor: "#0047BB",
+            backgroundColor: value === -1 ? "transparent" : "#0047BB",
           },
         }}
       >
@@ -21,7 +21,10 @@ export default function UserTabs({ value, onTabChange }) {
             sx={{
               color:
                 index === value ? "#E1E3E6 !important" : "#76787A !important",
-              fontWeight: index === value ? "bold" : "normal",
+              fontWeight: "600",
+              padding: "10px 16px",
+              borderBottom:
+                index !== value ? "1px solid #363738" : "2px solid transparent",
             }}
           />
         ))}
